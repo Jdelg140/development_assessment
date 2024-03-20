0424f8342f3e21f59d63bcda6ef6aa12f243ecc2
Jacob Delgado <128998284+Jdelg140@users.noreply.github.com>
2024-03-20 19:54:32 -0400

    Configured home page rendering and started Accordion + Popper Comp

---
+++ src/app/components/CommonComp/PopperComp/index.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Popper from '@mui/material/Popper';
+import { Button } from '@mui/material';
+
+export const PopperComp = () =>{
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(anchorEl ? null : event.currentTarget);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popper' : undefined;
+
+  return (
+    <Box>
+      <Button aria-describedby={id} type="button" onClick={handleClick}>
+        Toggle Popper
+      </Button>
+      <Popper id={id} open={open} anchorEl={anchorEl}>
+        <Box sx={{ border: 1, p: 1, bgcolor: 'background.paper' }}>
+          The content of the Popper.
+        </Box>
+      </Popper>
+    </Box>
+  );
+}
---
+++ src/app/components/Home/index.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { PopperComp } from "@/app/components/CommonComp/PopperComp/index";
+
+export const Home = () => {
+  return (
+    <>
+     <PopperComp />
+    </>
+  );
+};
