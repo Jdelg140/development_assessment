00c8bc4a562afa148f2ad8c18ad385b125be2c92
Jacob Delgado <128998284+Jdelg140@users.noreply.github.com>
2024-03-20 20:02:16 -0400

    Fixed styles file location and layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,9 +13,15 @@ export default function RootLayout({
       <head>
         <meta name="description" content="Experience More!" />
         <meta name="viewport" content="width=375px, initial-scale=1" />
-        <title>Bet Mate</title>
+        <title>Document Upload Assessment</title>
         <link rel="icon" type="image/x-icon" sizes="16x16" href="" />
         <link rel="apple-touch-icon" sizes="180x180" href="" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Roboto+Flex:wght@400;600&display=swap"
+          rel="stylesheet"
+        />
       </head>
       <body style={root}>
         <div style={max}>{children}</div>
